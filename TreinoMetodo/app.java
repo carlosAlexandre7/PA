271f36b34ba0a5a01f57b5d6ba180723f068dedf
carlosAlexandre <142914111+carlosAlexandre7@users.noreply.github.com>
2024-05-08 12:10:21 -0300

    Add files via upload

---
+++ TreinoMetodo/app.java
@@ -0,0 +1,25 @@
+package packcadastro;
+import java.util.Scanner;
+public class app {
+
+	public static void main(String[] args) {
+    
+		Scanner in = new Scanner(System.in);
+		
+		
+    System.out.println("Digite seu nome completo, idade, peso e email nessa ordem");
+    
+    String nome = in.next();
+    int idade = in.nextInt();
+    int peso = in.nextInt();
+    String email = in.next();
+    
+    
+    
+    
+    
+    }
+
+	}
+
+
---
+++ TreinoMetodo/cadastrar.java
@@ -0,0 +1,20 @@
+package packcadastro;
+import java.util.Scanner;
+public class cadastrar {
+
+	Scanner in = new Scanner(System.in); 
+	public void cadastrarPessoa1() {
+		
+		System.out.println("entre com o nome:");
+		String nome = in.next();
+		System.out.println("entre com a idade:");
+	    int idade = in.nextInt();
+	    System.out.println("entre com o peso:");
+	    double peso = in.nextInt();
+	    System.out.println("entre com o email:");
+	    String email = in.next();
+		
+		
+	}
+	
+}
